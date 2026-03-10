825979092e684a0971011a0699324a9696415fa0
Zanny-Ch <mian.zain.dev@gmail.com>
2026-03-10 10:26:00 +0500

    Updating previous Logic.

--- include/player.h
+++ include/player.h
@@ -1,25 +1,21 @@
 #pragma once
+#include <string>
+#include "enemy.h"
 
 // Player Status Options
-enum class PlayerStatus
-{
-    Healthy,
-    Hurt,
-    Wounded,
-    Critical,
-    Dead
-};
+enum class PlayerStatus { Healthy, Hurt, Wounded, Critical, Dead };
 
 //Player data - all stats one player
 struct Player
 {
-    int health = 100;
-    int maxHealth = 100;
-    int gold = 0;
-    float speed = 1.0f;
-    bool alive = true;
-    char symbol = '@';
-    int room = 1;
+    int health              = 100;
+    int maxHealth           = 100;
+    int gold                = 0;
+    float speed             = 1.0f;
+    bool alive              = true;
+    char symbol             = '@';
+    int room                = 1;
+    Position position        = {3, 2}; //Default Starting Position
 };
 
 // Function declarations 
